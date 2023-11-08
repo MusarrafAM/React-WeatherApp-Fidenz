5229dee8eac9c77eba1bd6ee3cb174652c521b0b
Musarraf <muhammedmusarraf@gmail.com>
2023-11-08 16:27:09 +0530

    Added Full Responsiveness

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,7 +5,7 @@ const Body = (props) => {
 
   return (
     <div className="bg-[#1f2128] text-white">
-      <div className="grid grid-cols-2 max-w-[1140px] mx-auto gap-[40px] pb-8">
+      <div className="lg:grid grid-cols-2 max-w-[1140px] mx-auto gap-[40px] pb-8 pt-[100px]">
         <WeatherCard allCityDetails={props.allCityDetails[0]} color="blue-bg"/>
         <WeatherCard allCityDetails={props.allCityDetails[1]} color="purple-bg"/>
         <WeatherCard allCityDetails={props.allCityDetails[2]} color="green-bg"/>
