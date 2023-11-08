115759b70c64468d6580bd72a000613a8613b342
Musarraf <muhammedmusarraf@gmail.com>
2023-11-08 16:29:44 +0530

    Fixed a responsive bug

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,7 +5,7 @@ const Body = (props) => {
 
   return (
     <div className="bg-[#1f2128] text-white">
-      <div className="lg:grid grid-cols-2 max-w-[1140px] mx-auto gap-[40px] pb-8 pt-[100px]">
+      <div className="lg:grid grid-cols-2 max-w-[1140px] mx-auto gap-[40px] pb-8 pt-[100px] lg:pt-0">
         <WeatherCard allCityDetails={props.allCityDetails[0]} color="blue-bg"/>
         <WeatherCard allCityDetails={props.allCityDetails[1]} color="purple-bg"/>
         <WeatherCard allCityDetails={props.allCityDetails[2]} color="green-bg"/>
