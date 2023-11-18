30f229ea45d593c2944a6f09c1e3179d6545e000
Musarraf <muhammedmusarraf@gmail.com>
2023-11-19 03:24:37 +0530

    Dockerized the React App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import citiesData from "./cities.json";
-// import axios from "axios";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
